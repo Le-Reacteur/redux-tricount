62b5cf2d2500e867b331592f5120d79d2914ef0c
Etienne Dldc <etienne.dldc@outlook.fr>
2018-03-01 16:45:49 +0100

    Init

---
+++ src/components/App.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const App = () => <div>Hello React</div>;
+
+App.propTypes = {};
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css';
+// Note Milligram is small CSS theme
+import 'milligram';
+import './index.css';
+import { App } from './components/App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
