16c718a6a0905a7eed3c5ce7632af9047fbc9139
Etienne Dldc <etienne.dldc@outlook.fr>
2018-03-02 03:04:45 +0100

    Clean

--- src/index.js
+++ src/index.js
@@ -4,17 +4,13 @@ import 'normalize.css';
 // Note Milligram is small CSS theme
 import 'milligram';
 import './index.css';
-// import { App } from './views/App';
-// import { store } from './store';
-// import { Provider } from 'react-redux';
+import { App } from './views/App';
+import { store } from './store';
+import { Provider } from 'react-redux';
 
-import { Starter } from './views/Stater';
-
-// ReactDOM.render(
-//   <Provider store={store}>
-//   <App />,
-//   </Provider>,
-//   document.getElementById('root')
-// );
-
-ReactDOM.render(<Starter />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
