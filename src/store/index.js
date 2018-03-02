f92643ff65886aff700b901defd617b108e3c03a
Etienne Dldc <etienne.dldc@outlook.fr>
2018-03-02 10:39:32 +0100

    User reducer

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,5 @@
 import { createStore } from 'redux';
-
-// Identity reducer for now
-const reducer = (state = {}, action) => state;
+import { reducer } from './reducer';
 
 const devtool =
   process.env.NODE_ENV !== 'production' && window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
