d93947bb481b7cb453b8228018246f3103edf35f
Etienne Dldc <etienne.dldc@outlook.fr>
2018-03-02 17:08:35 +0100

    Add userId and update expenses list

--- src/selectors/index.js
+++ src/selectors/index.js
@@ -5,3 +5,16 @@ export function selectExpenses(state) {
 export function selectUsers(state) {
   return state.users;
 }
+
+export function selectExpensesWithUser(state) {
+  const users = selectUsers(state);
+  return state.expenses.map(expense => {
+    const user = users.find(user => {
+      return expense.userId === user.id;
+    });
+    return {
+      ...expense,
+      user,
+    };
+  });
+}
