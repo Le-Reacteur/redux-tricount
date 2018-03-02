ff0f473468ff73b1619399f4ec3dd50644b7d5eb
Etienne Dldc <etienne.dldc@outlook.fr>
2018-03-02 16:35:37 +0100

    Action

--- src/actions/index.js
+++ src/actions/index.js
@@ -9,3 +9,7 @@ export const addExpense = (amount, description) => {
     },
   };
 };
+
+export const REMOVE_EXPENSE = 'REMOVE_EXPENSE';
+
+export const removeExpense = expenseIndex => ({ type: REMOVE_EXPENSE, payload: expenseIndex });
