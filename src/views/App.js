bb82c2add8170da580b74cffb2a193b8d730d0f8
Etienne Dldc <etienne.dldc@outlook.fr>
2018-03-02 16:37:53 +0100

    Connect

--- src/views/App.js
+++ src/views/App.js
@@ -8,19 +8,29 @@ import { Card } from '../components/Card';
 import { List } from '../components/List';
 import { Expense } from '../components/Expense';
 import { NewExpenseForm } from './NewExpenseForm';
+import { removeExpense } from '../actions';
 
 const mapStateToProps = state => ({
   expenses: selectExpenses(state),
 });
 
-const AppRender = ({ expenses }) => {
+const mapDispatchToProps = {
+  removeExpense,
+};
+
+const AppRender = ({ expenses, removeExpense }) => {
   return (
     <Panels>
       <Panel>
         <Card title="Expenses">
           <List>
             {expenses.map((expense, index) => (
-              <Expense key={index} description={expense.description} amount={expense.amount} />
+              <Expense
+                key={index}
+                description={expense.description}
+                amount={expense.amount}
+                onRemove={() => removeExpense(index)}
+              />
             ))}
           </List>
         </Card>
@@ -34,6 +44,7 @@ AppRender.propTypes = {
   expenses: PropTypes.arrayOf(
     PropTypes.shape({ description: PropTypes.string.isRequired, amount: PropTypes.number.isRequired })
   ).isRequired,
+  removeExpense: PropTypes.func.isRequired,
 };
 
-export const App = connect(mapStateToProps)(AppRender);
+export const App = connect(mapStateToProps, mapDispatchToProps)(AppRender);
