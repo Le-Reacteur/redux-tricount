cbe63a12ec0c219c2e6ddb74322845562062a372
Etienne Dldc <etienne.dldc@outlook.fr>
2018-03-02 11:07:06 +0100

    Extract NewUserForm

--- src/views/UsersPanel.js
+++ src/views/UsersPanel.js
@@ -3,26 +3,22 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Panel } from '../components/Panel';
 import { Card } from '../components/Card';
-import { Button } from '../components/Button';
-import { ButtonContainer } from '../components/ButtonContainer';
 import { User } from '../components/User';
 import { List } from '../components/List';
-import { Input } from '../components/Input';
 import { CustomPropTypes, selectUsersWithSum } from '../selectors';
-import { extractDataFromSubmitEvent, clearFormFromSubmitEvent } from '../utils';
-import { addUser, removeUser } from '../actions';
+import { removeUser } from '../actions';
 import { CenteredText } from '../components/CenteredText';
+import { NewUserForm } from './NewUserForm';
 
 const mapStateToProps = state => ({
   usersWithSum: selectUsersWithSum(state),
 });
 
 const mapDispatchToProps = {
-  addUser,
   removeUser,
 };
 
-const UsersPanelRender = ({ usersWithSum, addUser, removeUser }) => {
+const UsersPanelRender = ({ usersWithSum, removeUser }) => {
   const noUsers = usersWithSum.length === 0;
   return (
     <Panel>
@@ -43,33 +39,13 @@ const UsersPanelRender = ({ usersWithSum, addUser, removeUser }) => {
           </List>
         )}
       </Card>
-      <form
-        onSubmit={e => {
-          e.preventDefault();
-          const data = extractDataFromSubmitEvent(e);
-          // Validate data
-          if (!data.username) {
-            alert('No name ?');
-            return;
-          }
-          addUser(data.username);
-          clearFormFromSubmitEvent(e);
-        }}
-      >
-        <Card title="New User">
-          <Input name="username" placeholder="User name" />
-        </Card>
-        <ButtonContainer>
-          <Button submit>Add User</Button>
-        </ButtonContainer>
-      </form>
+      <NewUserForm />
     </Panel>
   );
 };
 
 UsersPanelRender.propTypes = {
   usersWithSum: CustomPropTypes.usersWithSum.isRequired,
-  addUser: PropTypes.func.isRequired,
   removeUser: PropTypes.func.isRequired,
 };
 
