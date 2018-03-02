f1dbc059c1c03828f2b119541d8b76f6a8bc5100
Etienne Dldc <etienne.dldc@outlook.fr>
2018-03-02 12:11:31 +0100

    Merge

--- src/store/reducer/expensesReducer.js
+++ src/store/reducer/expensesReducer.js
@@ -1,47 +1,28 @@
-import { initialState as users } from './usersReducer';
-import { ADD_EXPENSE, REMOVE_USER, REMOVE_EXPENSE } from '../../actions';
-
-const randomUserId = () => users[Math.floor(users.length * Math.random())].id;
 const randomAmount = () => Math.floor(Math.random() * 10000) / 100;
 
 const initialState = [
   {
     amount: randomAmount(),
-    userId: randomUserId(),
-    description: 'First Expense',
+    description: 'Some expense',
   },
   {
     amount: randomAmount(),
-    userId: randomUserId(),
-    description: 'Second Expense',
+    description: 'Some expense',
   },
   {
     amount: randomAmount(),
-    userId: randomUserId(),
-    description: 'Third Expense',
+    description: 'Some expense',
   },
   {
     amount: randomAmount(),
-    userId: randomUserId(),
-    description: 'Fourth Expense',
+    description: 'Some expense',
   },
   {
     amount: randomAmount(),
-    userId: randomUserId(),
-    description: 'Fifth Expense',
+    description: 'Some expense',
   },
 ];
 
 export const expensesReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case ADD_EXPENSE:
-      return [...state, action.payload];
-    case REMOVE_EXPENSE:
-      return state.filter((v, i) => i !== action.payload);
-    // When a user is removed, we remove all it's expenses
-    case REMOVE_USER:
-      return state.filter(expense => expense.userId !== action.payload);
-    default:
-      return state;
-  }
+  return state;
 };
