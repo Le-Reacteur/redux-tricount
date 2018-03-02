3b61ff53365542841e1ebd70e79224d31f41f805
Etienne Dldc <etienne.dldc@outlook.fr>
2018-03-02 10:13:25 +0100

    Devtool

--- src/store/index.js
+++ src/store/index.js
@@ -3,4 +3,7 @@ import { createStore } from 'redux';
 // Identity reducer for now
 const reducer = (state = {}, action) => state;
 
-export const store = createStore(reducer);
+const devtool =
+  process.env.NODE_ENV !== 'production' && window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+export const store = createStore(reducer, devtool);
