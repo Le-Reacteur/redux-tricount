7e0cd4e11cd8683cfac3a45507ff87a3b0e7649d
Etienne Dldc <etienne.dldc@outlook.fr>
2018-03-02 12:05:48 +0100

    Fix User

--- src/components/User.js
+++ src/components/User.js
@@ -13,7 +13,7 @@ export const User = ({ name, sum, color, onRemove }) => {
     <div className="user">
       <div className="user--img" style={{ background: color }} />
       <p className="user--name">{name}</p>
-      {sum && <Amount value={sum} className={onRemove ? 'user--amount' : ''} />}
+      {sum !== undefined && <Amount value={sum} className={onRemove ? 'user--amount' : ''} />}
       {onRemove && (
         <Button flatStyle dangerStyle className="user--delete" onClick={onRemove}>
           Delete
