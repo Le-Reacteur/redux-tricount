cec9b17ce038779076efc2cdd55557dbbf3113dc
Etienne Dldc <etienne.dldc@outlook.fr>
2018-03-02 09:53:18 +0100

    Clean

--- src/index.js
+++ src/index.js
@@ -4,13 +4,5 @@ import 'normalize.css';
 // Milligram is small CSS theme
 import 'milligram';
 import './index.css';
-import { App } from './views/App';
-import { store } from './store';
-import { Provider } from 'react-redux';
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-);
+ReactDOM.render(<div>Hello React</div>, document.getElementById('root'));
--- src/store/reducer/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import { expensesReducer } from './expensesReducer';
-import { usersReducer } from './usersReducer';
-
-export const reducer = combineReducers({
-  users: usersReducer,
-  expenses: expensesReducer,
-});
