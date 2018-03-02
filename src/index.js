0fa693b8b1ffa3be9133b34461f678034bc5497f
Etienne Dldc <etienne.dldc@outlook.fr>
2018-03-02 09:49:58 +0100

    Fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'normalize.css';
-// Note Milligram is small CSS theme
+// Milligram is small CSS theme
 import 'milligram';
 import './index.css';
 import { App } from './views/App';
