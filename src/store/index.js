5e9276206d3146af18eac8eb660b10d05b900f10
Etienne Dldc <etienne.dldc@outlook.fr>
2018-03-02 09:53:18 +0100

    Clean

--- src/store/index.js
+++
@@ -1,7 +0,0 @@
-import { createStore } from 'redux';
-import { reducer } from './reducer';
-
-const devtool =
-  process.env.NODE_ENV !== 'production' && window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
-
-export const store = createStore(reducer, devtool);
