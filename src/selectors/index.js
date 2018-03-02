4fbb450975b2008b93e126497ad54345f8db9c6f
Etienne Dldc <etienne.dldc@outlook.fr>
2018-03-02 16:45:26 +0100

    Selector

--- src/selectors/index.js
+++ src/selectors/index.js
@@ -1,3 +1,11 @@
 export function selectExpenses(state) {
   return state.expenses;
 }
+
+export function selectSum(state) {
+  let sum = 0;
+  state.expenses.forEach(expense => {
+    sum = sum + expense.amount;
+  });
+  return sum;
+}
